6118e900d5817268e5552d4b606260d55f96a06a
karen1993t <avtandilyan1993t@gmail.com>
2021-09-13 23:16:35 +0400

    add gradle

---
+++ app/src/main/java/com/armenia_guide/auth_client/AuthClientFragment.kt
@@ -0,0 +1,27 @@
+package com.armenia_guide.auth_client
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.armenia_guide.databinding.FragmentAuthClientBinding
+
+class AuthClientFragment : Fragment() {
+    private var showBindingAuthClient : FragmentAuthClientBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        showBindingAuthClient = FragmentAuthClientBinding.inflate(inflater,container,false)
+        return showBindingAuthClient?.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        showBindingAuthClient = null
+    }
+
+}
