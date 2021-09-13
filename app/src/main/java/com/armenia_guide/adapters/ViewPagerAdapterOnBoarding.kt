c7863e6a96ab8c300fc41bcd224c478869f92afd
beautyit <noiroxc@gmail.com>
2021-09-13 17:24:00 +0400

    Add fragment on boarding

---
+++ app/src/main/java/com/armenia_guide/adapters/ViewPagerAdapterOnBoarding.kt
@@ -0,0 +1,27 @@
+package com.armenia_guide.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.armenia_guide.on_boarding.*
+
+class ViewPagerAdapterOnBoarding(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    private val listFragment = listOf(
+        OnBoardingFragment1(),
+        OnBoardingFragment2(),
+        OnBoardingFragment3(),
+        OnBoardingFragment4(),
+        OnBoardingFragment5(),
+        OnBoardingFragment6()
+    )
+
+    override fun getItemCount(): Int {
+        return listFragment.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+       return listFragment[position]
+    }
+}
--- app/src/main/java/com/armenia_guide/on_boarding/OnBoardingFragment.kt
+++ app/src/main/java/com/armenia_guide/on_boarding/OnBoardingFragment.kt
@@ -1,26 +1,44 @@
 package com.armenia_guide.on_boarding
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.armenia_guide.R
+import androidx.viewpager2.widget.ViewPager2
+import com.armenia_guide.adapters.ViewPagerAdapterOnBoarding
+import com.armenia_guide.databinding.FragmentOnBoardingBinding
+
 
 
 class OnBoardingFragment : Fragment() {
 
-
+    private lateinit var adapter:ViewPagerAdapterOnBoarding
+   private var bindingOnBoardingFragment:FragmentOnBoardingBinding?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        adapter= ViewPagerAdapterOnBoarding(requireActivity().supportFragmentManager,lifecycle)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+        bindingOnBoardingFragment= FragmentOnBoardingBinding.inflate(inflater)
+
+
+
+
+
+        return bindingOnBoardingFragment?.root
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bindingOnBoardingFragment = null
     }
 
 }
