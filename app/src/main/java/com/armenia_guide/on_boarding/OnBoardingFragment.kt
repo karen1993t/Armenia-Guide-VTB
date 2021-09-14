8c4c413931ebadc80d766ce4a6381cb232eaf2c2
beautyit <noiroxc@gmail.com>
2021-09-14 11:52:43 +0400

    Add fragment on boarding

--- app/src/main/java/com/armenia_guide/on_boarding/OnBoardingFragment.kt
+++ app/src/main/java/com/armenia_guide/on_boarding/OnBoardingFragment.kt
@@ -1,31 +1,43 @@
 package com.armenia_guide.on_boarding
 
-import android.app.Activity
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.Fragment
 import com.armenia_guide.adapters.ViewPagerAdapterOnBoarding
 import com.armenia_guide.databinding.FragmentOnBoardingBinding
-
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class OnBoardingFragment : Fragment() {
 
-    private lateinit var adapter:ViewPagerAdapterOnBoarding
-   private var bindingOnBoardingFragment:FragmentOnBoardingBinding?= null
+    private lateinit var adapter: ViewPagerAdapterOnBoarding
+    private var bindingOnBoardingFragment: FragmentOnBoardingBinding? = null
+    val viewPager2 = bindingOnBoardingFragment?.viewPagerOnBoarding
+    val tabLayout = bindingOnBoardingFragment?.tabLayoutOnBoarding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter= ViewPagerAdapterOnBoarding(requireActivity().supportFragmentManager,lifecycle)
+        adapter = ViewPagerAdapterOnBoarding(requireActivity().supportFragmentManager, lifecycle)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        bindingOnBoardingFragment= FragmentOnBoardingBinding.inflate(inflater)
+        bindingOnBoardingFragment = FragmentOnBoardingBinding.inflate(inflater)
+
+        if (tabLayout != null && viewPager2 != null) {
+
+            TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            }.attach()
+
+        }
+
+
 
 
 
