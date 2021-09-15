9d023f582fc827292e574c37b844ccd55192aeeb
KAREN <avtandilyan1993t@gmail.com>
2021-09-15 16:41:51 +0400

    create ConfirmEmailFragment.kt

---
+++ app/src/main/java/com/armenia_guide/auth_client/ConfirmEmailFragment.kt
@@ -0,0 +1,28 @@
+package com.armenia_guide.auth_client
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.armenia_guide.databinding.FragmentConfirmEmailBinding
+
+class ConfirmEmailFragment : Fragment() {
+    private var showBindingConfirmEmail: FragmentConfirmEmailBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        showBindingConfirmEmail = FragmentConfirmEmailBinding.inflate(inflater, container, false)
+        return showBindingConfirmEmail?.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        showBindingConfirmEmail = null
+    }
+
+
+}
