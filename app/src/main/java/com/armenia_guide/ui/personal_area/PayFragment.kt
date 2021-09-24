e62aa0657191ce614c0a85db23b08490d9820e59
beautyit <noiroxc@gmail.com>
2021-09-24 17:59:17 +0400

    personal area

---
+++ app/src/main/java/com/armenia_guide/ui/personal_area/PayFragment.kt
@@ -0,0 +1,110 @@
+package com.armenia_guide.ui.personal_area
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.armenia_guide.databinding.FragmentPayBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
+
+typealias BarcodeListener = (barcode: String) -> Unit
+
+class PayFragment : Fragment() {
+    private var bindingPayFragment: FragmentPayBinding? = null
+    private lateinit var cameraExecutor: ExecutorService
+    private var processingBarcode = AtomicBoolean(false)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        bindingPayFragment = FragmentPayBinding.inflate(inflater)
+        return bindingPayFragment?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val requestPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){}
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions.launch(
+                arrayOf(Manifest.permission.CAMERA)
+            )
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(
+                    bindingPayFragment?.fragmentScanBarcodePreviewView?.surfaceProvider
+                )
+            }
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, BarcodeAnalyzer { barcode ->
+                        if (processingBarcode.compareAndSet(false, true)) {
+
+                            Toast.makeText(requireContext(),"$barcode",Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                // Unbind any bound use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to lifecycleOwner
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
+            } catch (e: Exception) {
+                Log.e("PreviewUseCase", "Binding failed! :(", e)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+
+
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bindingPayFragment = null
+    }
+
+}
--- app/src/main/java/com/armenia_guide/ui/personal_area/PersonalAreaFragment.kt
+++ app/src/main/java/com/armenia_guide/ui/personal_area/PersonalAreaFragment.kt
@@ -1,17 +1,20 @@
 package com.armenia_guide.ui.personal_area
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.armenia_guide.R
 import com.armenia_guide.databinding.FragmentPersonalAreaBinding
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import java.util.*
+import kotlin.collections.ArrayList
 
 class PersonalAreaFragment : Fragment() {
     private var bindingPersonalAreaFragment: FragmentPersonalAreaBinding? = null
@@ -33,57 +36,70 @@ class PersonalAreaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindingPersonalAreaFragment?.toolbar?.setNavigationIcon(R.drawable.ic_back_toolbar)
-        // bindingPersonalAreaFragment?.toolbar?.inflateMenu(R.menu.ic_help_toolbar)
+        // bindingPersonal
+        // AreaFragment?.toolbar?.inflateMenu(R.menu.ic_help_toolbar)
+
+
+        bindingPersonalAreaFragment?.pay?.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_personalAreaFragment_to_payFragment)
+        }
+
+
+        val monthList = resources.getStringArray(R.array.month)
+        val adapterMounts = ArrayAdapter(requireContext(), R.layout.drop_down_item_country,
+            R.id.text_view_drop_down,monthList)
+        bindingPersonalAreaFragment?.editMonth?.setAdapter(adapterMounts)
+
+        for (i in 0..Calendar.getInstance().time.month){
+            bindingPersonalAreaFragment?.month?.hint =monthList[i]
+        }
+
+
+        bindingPersonalAreaFragment?.editMonth?.setOnClickListener {
+            bindingPersonalAreaFragment?.month?.hint = ""
+
+            bindingPersonalAreaFragment?.editMonth?.showDropDown()
+        }
+
 
 
         val pieChart: PieChart = view.findViewById(R.id.piechart)
 
         val NoOfEmp = ArrayList<PieEntry>()
-
-        NoOfEmp.add(PieEntry(945f, 0))
-        NoOfEmp.add(PieEntry(1040f, 1))
-        NoOfEmp.add(PieEntry(1133f, 2))
-        NoOfEmp.add(PieEntry(1240f, 3))
-        NoOfEmp.add(PieEntry(1369f, 4))
-        NoOfEmp.add(PieEntry(1487f, 5))
-        NoOfEmp.add(PieEntry(1501f, 6))
+        NoOfEmp.add(PieEntry(296288f, 0))
+        NoOfEmp.add(PieEntry(45353f, 1))
+        NoOfEmp.add(PieEntry(50353f, 2))
+        NoOfEmp.add(PieEntry(45353f, 3))
+        NoOfEmp.add(PieEntry(57353f, 4))
 
 
         val dataSet = PieDataSet(NoOfEmp, "Number Of Employees")
 
-        val year = ArrayList<String>()
-        year.add("2008")
-        year.add("2009")
-        year.add("2010")
-        year.add("2011")
-        year.add("2012")
-        year.add("2013")
-        year.add("2014")
+        val price = ArrayList<String>()
+        price.add("AVIA")
+        price.add("RESTAURANTS")
+        price.add("HOTELS")
+        price.add("ALCOHOL")
+        price.add("OTHER")
+
 
         val colors = ArrayList<Int>()
-        colors.add(Color.RED)
-        colors.add(Color.YELLOW)
-        colors.add(Color.GREEN)
-        colors.add(Color.BLUE)
-        colors.add(Color.BLACK)
-        colors.add(Color.MAGENTA)
-        colors.add(Color.YELLOW)
+        colors.add(resources.getColor(R.color.color_1_personal_area, null))
+        colors.add(resources.getColor(R.color.color_2_personal_area, null))
+        colors.add(resources.getColor(R.color.color_3_personal_area, null))
+        colors.add(resources.getColor(R.color.color_4_personal_area, null))
+        colors.add(resources.getColor(R.color.color_5_personal_area, null))
+
 
         dataSet.colors = colors
-
-        //prabelner
-        dataSet.sliceSpace = 5f
 
 
         val data = PieData(dataSet)
 
+
         pieChart.data = data
 
         pieChart.animateXY(1000, 1000)
-
-
-        pieChart.description.text = "Values 2021"
-
 
     }
 
